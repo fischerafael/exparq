836795cecf31ab82b8a246ae71ed67dc8ff23b1f
Rafael Fischer <rafaelsanfischer@gmail.com>
2021-11-30 21:47:56 -0300

    finished landing page

--- src/pages/public/HomePage.tsx
+++ src/pages/public/HomePage.tsx
@@ -1,4 +1,6 @@
+import { Button } from "@chakra-ui/button";
 import { Flex, Text, VStack } from "@chakra-ui/layout";
+import { FaGoogle } from "react-icons/fa";
 
 export const HomePage = () => {
   return (
@@ -6,16 +8,34 @@ export const HomePage = () => {
       w="100vw"
       h="100vh"
       justify="center"
-      fontFamily="sans-serif"
       textAlign="center"
+      color="gray.900"
     >
-      <VStack w="full" maxW="lg">
-        <Text fontWeight="bold">Ux Arch</Text>
-        <Text fontSize="xl">Projete Experiências Arquitetônicas</Text>
-        <Text>
-          O Ux Arch utiliza Inteligência Artificial para ajudar arquitetos a
-          preverem como os usuários se sentirão nos espaços projetados.
-        </Text>
+      <VStack w="full" maxW="lg" bg="white" px="8">
+        <Flex h="30vh" align="center">
+          <Text fontWeight="extrabold">Ux Arch</Text>
+        </Flex>
+
+        <VStack h="70vh" spacing="8" justify="center">
+          <Text fontSize="4xl" fontWeight="extrabold" lineHeight="1.15">
+            Projete Experiências Arquitetônicas
+          </Text>
+          <Text>
+            O Ux Arch utiliza Inteligência Artificial para ajudar arquitetos a
+            preverem nos estágios iniciais do projeto como usuários irão se
+            sentir nos espaços concebidos.
+          </Text>
+
+          <Button
+            leftIcon={<FaGoogle />}
+            borderRadius="sm"
+            colorScheme="blue"
+            size="lg"
+            variant="solid"
+          >
+            Projetar
+          </Button>
+        </VStack>
       </VStack>
     </Flex>
   );
