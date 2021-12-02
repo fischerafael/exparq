da2f1b2d3de5692c752bc3a5a7d207400ba49aa9
Rafael Fischer <rafaelsanfischer@gmail.com>
2021-12-02 20:51:12 -0300

    add breadcrumb

--- src/pages/app/references/index.tsx
+++ src/pages/app/references/index.tsx
@@ -1,9 +1,13 @@
-import { IconButton } from "@chakra-ui/button";
-import { Flex, Text } from "@chakra-ui/layout";
-import { HiOutlineChevronLeft } from "react-icons/hi";
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+} from "@chakra-ui/breadcrumb";
+import NextLink from "next/link";
+import { Flex } from "@chakra-ui/layout";
+import { HiOutlineChevronRight } from "react-icons/hi";
 import { Header } from "../../../components/organisms/Header";
 import { AppTemplate } from "../../../components/templates/AppTemplate";
-import { handleNavigate } from "../../../utils/handleNavigate";
 
 export const ReferencesPage = () => {
   return (
@@ -11,20 +15,26 @@ export const ReferencesPage = () => {
       header={<Header />}
       body={
         <Flex h="full" spacing="8" justify="center" align="flex-start" w="full">
-          <Flex w="full" justify="space-between" h="5vh" align="center">
-            <Text fontWeight="bold" fontSize="lg">
-              Referências
-            </Text>
+          <Flex
+            w="full"
+            justify="space-between"
+            h="5vh"
+            align="center"
+            color="gray.500"
+          >
+            <Breadcrumb separator={<HiOutlineChevronRight />}>
+              <BreadcrumbItem>
+                <BreadcrumbLink as={NextLink} href="/app">
+                  App
+                </BreadcrumbLink>
+              </BreadcrumbItem>
 
-            <IconButton
-              aria-label="Logout"
-              icon={<HiOutlineChevronLeft />}
-              borderRadius="full"
-              colorScheme="blue"
-              variant="ghost"
-              size="sm"
-              onClick={() => handleNavigate("/app")}
-            />
+              <BreadcrumbItem>
+                <BreadcrumbLink as={NextLink} href="/app/references">
+                  Referências
+                </BreadcrumbLink>
+              </BreadcrumbItem>
+            </Breadcrumb>
           </Flex>
         </Flex>
       }
