1c65c0a6b37dbf1107c728fc2af4d4f620c97709
Rafael Fischer <rafaelsanfischer@gmail.com>
2021-12-02 20:56:30 -0300

    add references top

--- src/pages/app/references/index.tsx
+++ src/pages/app/references/index.tsx
@@ -1,20 +1,27 @@
+import NextLink from "next/link";
 import {
   Breadcrumb,
   BreadcrumbItem,
   BreadcrumbLink,
 } from "@chakra-ui/breadcrumb";
-import NextLink from "next/link";
-import { Flex } from "@chakra-ui/layout";
-import { HiOutlineChevronRight } from "react-icons/hi";
+import { Flex, Text, VStack } from "@chakra-ui/layout";
+import { HiOutlineChevronRight, HiOutlinePlus } from "react-icons/hi";
 import { Header } from "../../../components/organisms/Header";
 import { AppTemplate } from "../../../components/templates/AppTemplate";
+import { IconButton } from "@chakra-ui/button";
 
 export const ReferencesPage = () => {
   return (
     <AppTemplate
       header={<Header />}
       body={
-        <Flex h="full" spacing="8" justify="center" align="flex-start" w="full">
+        <VStack
+          h="full"
+          spacing="8"
+          justify="flex-start"
+          align="flex-start"
+          w="full"
+        >
           <Flex
             w="full"
             justify="space-between"
@@ -36,7 +43,29 @@ export const ReferencesPage = () => {
               </BreadcrumbItem>
             </Breadcrumb>
           </Flex>
-        </Flex>
+
+          <Flex
+            w="full"
+            justify="space-between"
+            h="5vh"
+            align="center"
+            color="gray.500"
+          >
+            <Text fontWeight="bold" fontSize="xl" color="gray.900">
+              Referências
+            </Text>
+
+            <IconButton
+              aria-label="Logout"
+              icon={<HiOutlinePlus />}
+              borderRadius="sm"
+              colorScheme="blue"
+              size="lg"
+            >
+              Sair
+            </IconButton>
+          </Flex>
+        </VStack>
       }
     />
   );
