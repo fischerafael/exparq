3bd6ebaccd0cb585ff3f697e835ae9b45317c273
Rafael Fischer <rafaelsanfischer@gmail.com>
2021-12-09 21:07:27 -0300

    revert to brain.js

--- pages/api/predict/index.ts
+++ pages/api/predict/index.ts
@@ -1,4 +1,5 @@
 import { NextApiRequest, NextApiResponse } from "next";
+import brain from "brain.js";
 
 export default async function handler(
   req: NextApiRequest,
@@ -8,7 +9,22 @@ export default async function handler(
 
   if (method === "POST") {
     try {
-      return res.status(201).json(body);
+      const { value1, value2 } = body;
+
+      const net = new brain.NeuralNetwork({ hiddenLayers: [3] });
+
+      const trainingData = [
+        { input: [0, 0], output: [0] },
+        { input: [0, 1], output: [1] },
+        { input: [1, 0], output: [1] },
+        { input: [1, 1], output: [0] },
+      ];
+
+      net.train(trainingData);
+
+      const result = net.run([+value1, +value2]);
+
+      return res.status(201).json(result);
     } catch (e: any) {
       return res.status(400).json({ message: e.message });
     }
