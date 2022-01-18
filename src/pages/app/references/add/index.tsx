59d98760a228f424703cd12d2bb9912d88cb3c43
Rafael Fischer <rafaelsanfischer@gmail.com>
2022-01-18 16:28:03 -0300

    removed old add reference components

--- src/pages/app/references/add/index.tsx
+++ src/pages/app/references/add/index.tsx
@@ -1,30 +1,9 @@
-import NextLink from "next/link";
-import {
-  Breadcrumb,
-  BreadcrumbItem,
-  BreadcrumbLink,
-} from "@chakra-ui/breadcrumb";
 import Router from "next/router";
 import { Flex, Text, VStack } from "@chakra-ui/react";
-import { HiOutlineChevronRight, HiOutlineX } from "react-icons/hi";
 import { Header } from "../../../../components/organisms/Header";
 import { AppTemplate } from "../../../../components/templates/AppTemplate";
-import { Button, IconButton } from "@chakra-ui/button";
-import { handleNavigate } from "../../../../utils/handleNavigate";
-import { Image } from "@chakra-ui/image";
-import { Input } from "../../../../components/molecules/Input";
-import { SelectInput } from "../../../../components/molecules/SelectInput";
-import {
-  height,
-  shape,
-  size,
-  complexity,
-  materials,
-  textures,
-  tones,
-  colors,
-} from "../../../../constants/options";
-import { useEffect, useState } from "react";
+import { Button } from "@chakra-ui/button";
+import { useState } from "react";
 
 import { UsersSection } from "../../../../components/organisms/Projects/UsersSection";
 import { ContextSection } from "../../../../components/organisms/Projects/ContextSection";
@@ -37,6 +16,10 @@ import { ImageSection } from "../../../../components/organisms/Projects/ImageSec
 import { BreadCrumb } from "../../../../components/organisms/BreadCrumb";
 import { breadcrumbs } from "../../../../constants/breadCrumb";
 import { PageHeader } from "../../../../components/organisms/Projects/PageHeader";
+import { GeneralSection } from "../../../../components/organisms/Projects/GeneralSection";
+import { ShapeSection } from "../../../../components/organisms/Projects/ShapeSection";
+import { MaterialsSection } from "../../../../components/organisms/Projects/MaterialsSection";
+import { ColorSection } from "../../../../components/organisms/Projects/ColorSection";
 
 export const AddReferencePage = () => {
   const projectType = "reference";
@@ -156,147 +139,29 @@ export const AddReferencePage = () => {
 
           <ImageSection image={generalInfo.image} />
 
-          <VStack h="full" w="full" spacing="4" align="flex-start">
-            <Text fontWeight="bold">1. Informações Gerais</Text>
+          <GeneralSection
+            sectionTitle="1. Informações Gerais"
+            state={generalInfo}
+            setState={setGeneralInfo}
+          />
 
-            <Input
-              label="Imagem da Referência (URL)"
-              placeholder="Ex: www.google.com/images/projeto.jpg"
-              value={generalInfo.image}
-              onChange={(e) =>
-                setGeneralInfo({ ...generalInfo, image: e.target.value })
-              }
-            />
-            <Input
-              label="Nome da Referência"
-              placeholder="Ex: Casa 6"
-              value={generalInfo.name}
-              onChange={(e) =>
-                setGeneralInfo({ ...generalInfo, name: e.target.value })
-              }
-            />
-            <Input
-              label="Localização da Referência"
-              placeholder="Ex: Curitiba, Paraná"
-              value={generalInfo.location}
-              onChange={(e) =>
-                setGeneralInfo({ ...generalInfo, location: e.target.value })
-              }
-            />
-          </VStack>
+          <ShapeSection
+            sectionTitle="2. Forma"
+            state={shapeInfo}
+            setState={setShapeInfo}
+          />
 
-          <VStack h="full" w="full" spacing="4" align="flex-start">
-            <Text fontWeight="bold">2. Forma</Text>
+          <MaterialsSection
+            sectionTitle="3. Materiais e Texturas"
+            state={materialsAndContrast}
+            setState={setMaterialsAndContrast}
+          />
 
-            <SelectInput
-              label="Altura"
-              options={height}
-              value={shapeInfo.height}
-              onChange={(e) =>
-                setShapeInfo({ ...shapeInfo, height: +e.target.value })
-              }
-            />
-            <SelectInput
-              label="Tamanho"
-              options={size}
-              value={shapeInfo.size}
-              onChange={(e) =>
-                setShapeInfo({ ...shapeInfo, size: +e.target.value })
-              }
-            />
-            <SelectInput
-              label="Complexidade Volumétrica"
-              options={complexity}
-              value={shapeInfo.complexity}
-              onChange={(e) =>
-                setShapeInfo({ ...shapeInfo, complexity: +e.target.value })
-              }
-            />
-            <SelectInput
-              label="Formas Dominantes"
-              options={shape}
-              value={shapeInfo.shape}
-              onChange={(e) =>
-                setShapeInfo({ ...shapeInfo, shape: +e.target.value })
-              }
-            />
-          </VStack>
-
-          <VStack h="full" w="full" spacing="4" align="flex-start">
-            <Text fontWeight="bold">3. Materiais e Contraste</Text>
-
-            <SelectInput
-              label="Materiais"
-              options={materials}
-              value={materialsAndContrast.materials}
-              onChange={(e) =>
-                setMaterialsAndContrast({
-                  ...materialsAndContrast,
-                  materials: +e.target.value,
-                })
-              }
-            />
-            <SelectInput
-              label="Texturas"
-              options={textures}
-              value={materialsAndContrast.texture}
-              onChange={(e) =>
-                setMaterialsAndContrast({
-                  ...materialsAndContrast,
-                  texture: +e.target.value,
-                })
-              }
-            />
-          </VStack>
-
-          <VStack h="full" w="full" spacing="4" align="flex-start">
-            <Text fontWeight="bold">4. Cores</Text>
-
-            <SelectInput
-              label="Tons"
-              options={tones}
-              value={colorsInfo.tone}
-              onChange={(e) =>
-                setColorsInfo({
-                  ...colorsInfo,
-                  tone: +e.target.value,
-                })
-              }
-            />
-            <SelectInput
-              label="Cor Primária"
-              options={colors}
-              value={colorsInfo.primaryColor}
-              onChange={(e) =>
-                setColorsInfo({
-                  ...colorsInfo,
-                  primaryColor: +e.target.value,
-                })
-              }
-            />
-            <SelectInput
-              label="Cor Secundária"
-              options={colors}
-              value={colorsInfo.secondaryColor}
-              onChange={(e) =>
-                setColorsInfo({
-                  ...colorsInfo,
-                  secondaryColor: +e.target.value,
-                })
-              }
-            />
-            <SelectInput
-              label="Cor Terciária"
-              options={colors}
-              value={colorsInfo.tertiaryColor}
-              onChange={(e) =>
-                setColorsInfo({
-                  ...colorsInfo,
-                  tertiaryColor: +e.target.value,
-                })
-              }
-            />
-          </VStack>
+          <ColorSection
+            sectionTitle="4. Cores"
+            state={colorsInfo}
+            setState={setColorsInfo}
+          />
 
           <LightSection
             sectionTitle="5. Iluminação"
