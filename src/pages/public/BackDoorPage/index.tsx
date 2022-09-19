ff66672f198fcee540c4cbf1b5c460185ae0b82f
Rafael Fischer <rafaelsanfischer@gmail.com>
2022-09-19 13:32:32 -0300

    backdoor access

--- src/pages/public/BackDoorPage/index.tsx
+++ src/pages/public/BackDoorPage/index.tsx
@@ -1,18 +1,28 @@
-import { Input } from "../../../components/molecules/Input";
 import { AppTemplate } from "../../../components/templates/AppTemplate";
+import { useState } from "react";
 
 import { useSession } from "../../../contexts/useSession";
 
 export const BackDoorPage = () => {
   const { handleManualLogin } = useSession();
 
+  const [email, setEmail] = useState("");
+
+  const handleLogin = () => {
+    handleManualLogin(email);
+  };
+
   return (
     <AppTemplate
       header={<p>backdoor</p>}
       body={
         <>
-          <Input label="Email" />
-          <button>Entrar</button>
+          <input
+            value={email}
+            placeholder="email"
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <button onClick={handleLogin}>Entrar</button>
         </>
       }
     />
